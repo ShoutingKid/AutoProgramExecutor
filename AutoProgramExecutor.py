3af7f619bf40d346b19721e1ec93682bdb269223
Om3ga <91276621+ShoutingKid@users.noreply.github.com>
2021-12-12 16:38:37 +0530

    Update AutoProgramExecutor.py

--- AutoProgramExecutor.py
+++ AutoProgramExecutor.py
@@ -12,5 +12,6 @@ time.sleep(0.1)
 pyauto.write("cd python")
 pyauto.press("enter")
 time.sleep(0.1)
-pyauto.write("python donut.py")
+pyauto.write("python donut.py") #You can specify the file you want file name
 pyauto.press("enter")
+ # Note : I set the time.sleep() as 0.1 so that it is fast and our work is ACTUALLY fast
